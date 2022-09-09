9fdf4fb98d1ac537afd937c01e223f612978a916
gabriel <eshumomoh1@hotmail.com>
2022-09-08 20:55:37 -0700

    Task 4 now

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - printing
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+puts("\"Programming is like building a multiligual puzzle");
+	    return (0);
+
+}
+
+
