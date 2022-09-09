17f90758f18a323cf33338f7f4e1d20a24d68211
gabriel <eshumomoh1@hotmail.com>
2022-09-08 21:03:15 -0700

    Task 5 now

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints a text
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+printf("with proper grammar bbut the outcome is a piece of art,\n");
+	    return (0);
+
+}
