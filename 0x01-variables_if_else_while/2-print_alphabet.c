4925b3e4ec72f8103afcaada3ce7f2a436856656
gabriel <eshumomoh1@hotmail.com>
2022-09-12 18:41:29 -0700

    My task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Description: using the main function
+ * this program prints alphabet in lower case
+ * Return 0
+ */
+int main(void)
+{
+	putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
